8dbc19c1028d5536c6878b440be9c46761089ae0
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-06-08 18:27:08 +0530

    created a filem path and joint files with server to show data in files with by importing fs, url, path modules

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-        res.writeHead(200, {"Content-Type" : "text/html"});
-        res.end("<h1>Homepage</h1>");
-    }else if(req.url === '/about'){
-        res.writeHead(200, {"Content-Type" : "text/html"});
-        res.end("<h1>About</h1>");
-    }else{
-        res.writeHead(404, {"Content-Type" : "text/html"});
-        res.end("<h1>Not Found!</h1>");
+//get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async (req, res) => {
+    try {
+        if(req.method === 'GET'){
+            let filePath;
+            if(req.url === '/'){
+                filePath = path.join(__dirname, 'public', 'index.html');
+            }else if(req.url === '/about'){
+                filePath = path.join(__dirname, 'public', 'about.html');
+            }else{
+                throw new Error('Not Found');
+            }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        }else{
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, {"Content-Type" : "text/plain"});
+                res.end("Server Error!");
     }
 });
 
