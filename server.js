88ef1c20f0fa02fc0b360831780bc507b57f409d
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-06-08 17:00:02 +0530

    checking url requests and making routers

---
+++ server.js
@@ -0,0 +1,19 @@
+import http from 'http';
+const PORT = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.end("<h1>Homepage</h1>");
+    }else if(req.url === '/about'){
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.end("<h1>About</h1>");
+    }else{
+        res.writeHead(404, {"Content-Type" : "text/html"});
+        res.end("<h1>Not Found!</h1>");
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
