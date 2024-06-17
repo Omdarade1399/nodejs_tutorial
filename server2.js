ba8af8e0a0b4f62aff6a85e602c4da6d9ddda777
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-06-17 14:55:54 +0530

    made the api with http module and could searsh perticular user by puttung id in url

--- server2.js
+++ server2.js
@@ -1,7 +1,7 @@
 import { createServer } from 'http';
 const PORT = process.env.PORT;
 
-const user = [
+const users = [
     {id: 1, name: 'John Doe'},
     {id: 2, name: 'Jane Doe'},
     {id: 3, name: 'Jim Doe'}
@@ -10,10 +10,24 @@ const user = [
 const server = createServer((req, res) => {
       if(req.url === '/api/users' && req.method === 'GET') {
         res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(user));
+        res.write(JSON.stringify(users));
         res.end();
+      }else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+        const id = req.url.split('/')[3];
+        const user = users.find((user) => user.id === parseInt(id));
+        if (user) {
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify(user));
+            res.end();
+        }else {
+            res.setHeader('Content-Type', 'application/json');
+            res.statusCode = 404;
+            res.write(JSON.stringify({Message: 'User not found'}));
+            res.end();
+        }
       }else {
         res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 404;
         res.write(JSON.stringify({Message: 'Route not found'}));
         res.end();
       }
