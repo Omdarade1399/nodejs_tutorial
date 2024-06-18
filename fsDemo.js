026a44e13daf5fb59f7e8eaad54089d4017ead56
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-06-18 20:57:05 +0530

    synchronous version for file system module

--- fsDemo.js
+++ fsDemo.js
@@ -1,7 +1,12 @@
 import fs from 'fs';
 
-// readFile() - callback
+// readFile() - callback version
 fs.readFile('./test.txt', 'utf8', (err, data) => { 
     if(err) throw err;
     console.log(data);
 });
+
+//readFileSync() - synchronous version
+
+const data = fs.readFileSync('./test.txt', 'utf8');
+console.log(data);
