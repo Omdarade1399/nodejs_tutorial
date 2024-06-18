e60f366dd59c3425bab74f63a34a2fd4690a5ea5
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-06-18 21:04:24 +0530

    promise .then() version for file system module

--- fsDemo.js
+++ fsDemo.js
@@ -1,12 +1,17 @@
-import fs from 'fs';
+// import fs from 'fs';
+import fs from 'fs/promises';
 
 // readFile() - callback version
-fs.readFile('./test.txt', 'utf8', (err, data) => { 
-    if(err) throw err;
-    console.log(data);
-});
+// fs.readFile('./test.txt', 'utf8', (err, data) => { 
+//     if(err) throw err;
+//     console.log(data);
+// });
 
 //readFileSync() - synchronous version
+// const data = fs.readFileSync('./test.txt', 'utf8');
+// console.log(data);
 
-const data = fs.readFileSync('./test.txt', 'utf8');
-console.log(data);
+// readFile() - promise .then() version
+fs.readFile('./test.txt', 'utf8')
+ .then((data) => console.log(data))
+ .catch((err) => console.log(err));
